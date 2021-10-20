50b48aeccbca9d6d25f473accb52485875a4455f
moore-brandon4 <moore_brandon4@columbusstate.edu>
2021-10-20 23:43:36 +0000

    project

--- complete/src/main/java/com/example/springboot/HelloController.java
+++ complete/src/main/java/com/example/springboot/HelloController.java
@@ -3,6 +3,7 @@ package com.example.springboot;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.ui.Model;
 
 @Controller
@@ -24,10 +25,28 @@ public class HelloController {
 		return "xkcd";
 	}
 
+	@GetMapping("/nasa")
+	public String nasa(@RequestParam(name="id", required=false) String id, Model model) {
+		if(id==null){
+			id=java.time.LocalDateTime.now();
+		}
+		model.addAttribute("id", id);
+		return "nasa";
+	}
+
 	@GetMapping("/xkcdSpec")
-	public String xkcdSpecific(@RequestParam(name="id", required=true) String id, Model model) {
+	public String xkcdSpecific(@RequestParam(name="id", required=false) String id, Model model) {
+		if(id==null){
+			id="200";
+		}
 		model.addAttribute("id", id);
 		return "xkcdSpecific";
 	}
+
+	// @GetMapping("/xkcdSpec/{id}")
+	// public String xkcdSpecific(@PathVariable String id, Model model) {
+	// 	model.addAttribute("id", id);
+	// 	return "xkcdSpecific";
+	// }
 
 }
